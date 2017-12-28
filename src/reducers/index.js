92478ef9448540ec0b65b348ca334da78ea88958
Alice Borges <aliceborges@outlook.com.br>
2017-12-27 23:13:10 -0200

    feat: finalização com conexão à API

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,21 +1,22 @@
 import { combineReducers } from 'redux'
-import{
-	ADD_RECIPE,
-	REMOVE_FROM_CALENDAR
+
+import {
+  ADD_RECIPE,
+  REMOVE_FROM_CALENDAR,
 } from '../actions'
 
-function food (state={}, action){
-	switch (action.type) {
-		case ADD_RECIPE:
-			const {recipe} = action
+function food (state = {}, action) {
+  switch (action.type) {
+    case ADD_RECIPE :
+      const { recipe } = action
 
-			return{
-				...state,
-				[recipe.label]: recipe
-			}
-		default:
-			return state
-	}
+      return {
+        ...state,
+        [recipe.label]: recipe,
+      }
+    default :
+      return state
+  }
 }
 
 const initialCalendarState = {
@@ -56,32 +57,32 @@ const initialCalendarState = {
   },
 }
 
-function calendar(state= initialCalendarState, action){
-	const {day, recipe, meal} = action
+function calendar (state = initialCalendarState, action) {
+  const { day, recipe, meal } = action
 
-	switch(action.type){
-		case ADD_RECIPE:
-			return {
-				...state,
-				[day]: {
-					...state[day],
-					[meal]: recipe.label,
-				}
-			}
-		case REMOVE_FROM_CALENDAR:
-			return {
-				...state,
-				[day]: {
-					...state[day],
-					[meal]: null,
-				}
-			}
-		default:
-			return state;
-	}
+  switch (action.type) {
+    case ADD_RECIPE :
+      return {
+        ...state,
+        [day]: {
+          ...state[day],
+          [meal]: recipe.label,
+        }
+      }
+    case REMOVE_FROM_CALENDAR :
+      return {
+        ...state,
+        [day]: {
+          ...state[day],
+          [meal]: null,
+        }
+      }
+    default :
+      return state
+  }
 }
 
 export default combineReducers({
-	food,
-	calendar
-});
+  food,
+  calendar,
+})
--- src/utils/api.js
+++ src/utils/api.js
@@ -1,11 +1,10 @@
-
-const API_ID = process.env.aliceborges_
-const APP_KEY = process.env.udacity
+const API_ID = process.env.REACT_APP_API_ID
+const APP_KEY = process.env.REACT_APP_APP_KEY
 
 export function fetchRecipes (food = '') {
   food = food.trim()
 
-  return fetch(`https://api.edamam.com/search?q=${food}&app_id=${API_ID}&app_key=${APP_KEY}`)
+  return fetch(`https://api.edamam.com/search?q=${food}&app_id=9df48134&app_key=2633dff48c52ad278d56899982c33693`)
     .then((res) => res.json())
     .then(({ hits }) => hits.map(({ recipe }) => recipe))
 }
