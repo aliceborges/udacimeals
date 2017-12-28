6a36d374af04d9be8be1138ca5541d9fc328c929
Alice Borges <aliceborges@outlook.com.br>
2017-12-27 22:00:36 -0200

    feat: adição de pacotes e configuração dos modals e icons

---
+++ src/utils/api.js
@@ -0,0 +1,11 @@
+
+const API_ID = process.env.aliceborges_
+const APP_KEY = process.env.udacity
+
+export function fetchRecipes (food = '') {
+  food = food.trim()
+
+  return fetch(`https://api.edamam.com/search?q=${food}&app_id=${API_ID}&app_key=${APP_KEY}`)
+    .then((res) => res.json())
+    .then(({ hits }) => hits.map(({ recipe }) => recipe))
+}
